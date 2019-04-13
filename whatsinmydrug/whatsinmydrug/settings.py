befc0510577cdff89105ae05fc683cdb5cec46a6
AnakinM <mateusz.anikiej@gmail.com>
2019-04-13 12:54:58 +0100

    mainview app and first view

--- whatsinmydrug/whatsinmydrug/settings.py
+++ whatsinmydrug/whatsinmydrug/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'mainview.apps.MainviewConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
