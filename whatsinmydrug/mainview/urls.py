dbcc0b1ba2cff0f23029df84ca7f8bb342cccd32
AnakinM <mateusz.anikiej@gmail.com>
2019-04-13 15:52:47 +0100

    added input for substances

--- whatsinmydrug/mainview/urls.py
+++ whatsinmydrug/mainview/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, get_drug_names
 
 urlpatterns = [
     path('', home, name='mainview-home'),
+    path('search/', get_drug_names, name='search-drugs'),
 ]
--- whatsinmydrug/mainview/views.py
+++ whatsinmydrug/mainview/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'mainview/base.html')
+    return render(request, 'mainview/homepage.html')
 
+def get_drug_names(request):
+    user_input = request.POST['substances']
+    substances = user_input.split(',')
+    print(substances)
+    return render(request, 'mainview/homepage.html')
